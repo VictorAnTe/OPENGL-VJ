5fb90d1718ff506e608849700544696735ded693
APV23232 <118616904+APV23232@users.noreply.github.com>
2025-09-11 11:59:11 +0200

    Player.cpp and Player.h

---
+++ 2DGame/03-MetalGear/03-MetalGear/Player.cpp
@@ -0,0 +1,136 @@
+#include <cmath>
+#include <iostream>
+#include <GL/glew.h>
+#include "Player.h"
+#include "Game.h"
+#include <glm/glm.hpp>
+
+
+#define JUMP_ANGLE_STEP 4
+#define JUMP_HEIGHT 96
+#define FALL_STEP 4
+#define PLAYER_HEALTH 100
+
+enum PlayerAnims
+{
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+};
+
+void Player::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram)
+{
+	bJumping = false;
+	spritesheet.loadFromFile("images/bub.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
+	// Poner 4 animaciones (Cambiar-se porque habra mas aniaciones)
+	sprite->setNumberAnimations(4);
+
+	// STAND_LEFT
+	sprite->setAnimationSpeed(STAND_LEFT, 8);
+	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
+
+	// STAND_RIGHT
+	sprite->setAnimationSpeed(STAND_RIGHT, 8);
+	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+
+	// MOVE_LEFT
+	sprite->setAnimationSpeed(MOVE_LEFT, 8);
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
+
+	// MOVE_RIGHT
+	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+
+	sprite->changeAnimation(0);
+	tileMapDispl = tileMapPos;
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+
+}
+
+void Player::update(int deltaTime)
+{
+	sprite->update(deltaTime);
+	if (Game::instance().getKey(GLFW_KEY_LEFT))
+	{
+		if (sprite->animation() != MOVE_LEFT)
+			sprite->changeAnimation(MOVE_LEFT);
+		posPlayer.x -= 2;
+		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+		{
+			posPlayer.x += 2;
+			sprite->changeAnimation(STAND_LEFT);
+		}
+	}
+	else if (Game::instance().getKey(GLFW_KEY_RIGHT))
+	{
+		if (sprite->animation() != MOVE_RIGHT)
+			sprite->changeAnimation(MOVE_RIGHT);
+		posPlayer.x += 2;
+		if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
+		{
+			posPlayer.x -= 2;
+			sprite->changeAnimation(STAND_RIGHT);
+		}
+	}
+	else
+	{
+		if (sprite->animation() == MOVE_LEFT)
+			sprite->changeAnimation(STAND_LEFT);
+		else if (sprite->animation() == MOVE_RIGHT)
+			sprite->changeAnimation(STAND_RIGHT);
+	}
+
+	if (bJumping)
+	{
+		jumpAngle += JUMP_ANGLE_STEP;
+		if (jumpAngle == 180)
+		{
+			bJumping = false;
+			posPlayer.y = startY;
+		}
+		else
+		{
+			posPlayer.y = int(startY - 96 * sin(3.14159f * jumpAngle / 180.f));
+			if (jumpAngle > 90)
+				bJumping = !map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y);
+		}
+	}
+	else
+	{
+		posPlayer.y += FALL_STEP;
+		if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y))
+		{
+			if (Game::instance().getKey(GLFW_KEY_UP))
+			{
+				bJumping = true;
+				jumpAngle = 0;
+				startY = posPlayer.y;
+			}
+		}
+	}
+
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
+// Renderizar el sprite del jugador
+void Player::render()
+{
+	sprite->render();
+}
+
+// Definir TileMap
+void Player::setTileMap(TileMap* tileMap)
+{
+	map = tileMap;
+}
+
+// Definir posicion en el TileMap
+void Player::setPosition(const glm::vec2& pos)
+{
+	posPlayer = pos;
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
---
+++ 2DGame/03-MetalGear/03-MetalGear/Player.h
@@ -0,0 +1,33 @@
+#ifndef _PLAYER_INCLUDE
+#define _PLAYER_INCLUDE
+
+
+#include "Sprite.h"
+#include "TileMap.h"
+#include <glm/glm.hpp>
+
+// Player is basically a Sprite that represents the player. As such it has
+// all properties it needs to track its movement, jumping, and collisions.
+
+
+class Player
+{
+public:
+	void init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram);
+	void update(int deltaTime);
+	void render();
+
+	void setTileMap(TileMap* tileMap);
+	void setPosition(const glm::vec2& pos);
+
+private:
+	glm::ivec2 tileMapDispl, posPlayer;
+	int jumpAngle, startY;
+	bool damaged, unarmed;
+	Texture spritesheet;
+	Sprite* sprite;
+	TileMap* map;
+
+}
+
+#endif // _PLAYER_INCLUDE
